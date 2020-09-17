7c57ec48af50bccc8188631a18d84c014ae90bab
AMukherjee9 <AMukherjee9@slb.com>
2020-09-17 14:01:46 +0530

    Initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CdkDragStart, CdkDragEnd, CdkDragMove } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'grokonez-dragdrop';
+
+  state = '';
+  position = '';
+
+
+  onStarted(event: CdkDragStart) {
+    this.state = 'CdkDragStart';
+  }
+
+  onEnded(event: CdkDragEnd) {
+    this.state = 'CdkDragEnd';
+  }
+
+  onMoved(event: CdkDragMove) {
+    this.state = 'CdkDragMove';
+    this.position = `X: ${event.pointerPosition.x}, Y: ${event.pointerPosition.y}`;
+  }
 }
